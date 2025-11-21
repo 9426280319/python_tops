1f336e16a08d2de3976e54c5a70357ccf21b47e1
9426280319 <devratnani2027@gmail.com>
2025-11-21 06:42:02 +0530

    initial commit

---
+++ practice/basic/calculator.py
@@ -0,0 +1,27 @@
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = int(input("Enter choice: "))
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if choice == 1:
+    print("Result =", a + b)
+
+elif choice == 2:
+    print("Result =", a - b)
+
+elif choice == 3:
+    print("Result =", a * b)
+
+elif choice == 4:
+    if b != 0:
+        print("Result =", a / b)
+    else:
+        print("Cannot divide by zero")
+
+else:
+    print("Invalid choice")
